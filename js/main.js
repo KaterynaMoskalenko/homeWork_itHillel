bb2ad0c96b9d708c383811370c160a85d8861da3
Kateryna Moskalenko <emochkaom@gmail.com>
2025-02-07 14:29:11 +0200

    second assigment

--- js/main.js
+++ js/main.js
@@ -49,27 +49,27 @@ console.log('I am ' + typeof students_Ithillel);
 
 
 
-//let name = prompt('Enter your name, please', 'Kate');
-//let profession = prompt('Enter your specialty, please', 'developers');
-//let school = prompt('What courses did you complete as a developer?', 'itHillel')
+let name = prompt('Enter your name, please', 'Kate');
+let profession = prompt('Enter your specialty, please', 'developers');
+let school = prompt('What courses did you complete as a developer?', 'itHillel')
 
-// let template_Strings = `
-// Hello, my name is ${name}
-// I'd like to become a ${profession}
-// I have currently been  studying at school ${school}
-// `;
-// console.log(template_Strings);
+let template_Strings = `
+Hello, my name is ${name}
+I'd like to become a ${profession}
+I have currently been  studying at school ${school}
+`;
+console.log(template_Strings);
 
 /* +++ HW3 +++ */
 
-let number = 10369;
-number =`1 0 3 6 9`
-console.log(number)
-let number_2 = `1\t0\t3\t6\t9`
-console.log(number_2)
-console.log((10369).toString().split('').join(' '))
-let num_Str = '10369';
-console.log(num_Str.length)//5;
-console.log(num_Str[0]+' '+num_Str[1]+' '+num_Str[2]+" "+num_Str[3]+' '+num_Str[4]);
+// let number = 10369;
+// number =`1 0 3 6 9`
+// console.log(number)
+// let number_2 = `1\t0\t3\t6\t9`
+// console.log(number_2)
+// console.log((10369).toString().split('').join(' '))
+// let num_Str = '10369';
+// console.log(num_Str.length)//5;
+// console.log(num_Str[0]+' '+num_Str[1]+' '+num_Str[2]+" "+num_Str[3]+' '+num_Str[4]);
 
 
